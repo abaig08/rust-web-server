26268c5ac0fca714ae836f02b5b5ffb2afbe1946
Abdullah Baig <88853346+abaig08@users.noreply.github.com>
2024-07-09 17:22:23 +0530

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,16 +1,29 @@
-use actix_web::{web, App, HttpServer, Responder};
+use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::fs;
 
-async fn index() -> impl Responder {
-    "Hi there!"
-}
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-#[actix_rt::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    for stream in listener.incoming() {
+        let mut stream = stream.unwrap();
+
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+
+        let get = b"GET / HTTP/1.1\r\n";
+
+        let (status_line, filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+        } else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+
+        let contents = fs::read_to_string(filename).unwrap();
+
+        let response = format!("{}{}", status_line, contents);
+
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
 }
