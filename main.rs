43bbdf7f8e9a7d56f467bc72f7eded427d0d5861
Abdullah Baig <baigabdullah174@gmail.com>
2024-07-09 11:56:41 +0530

    Create main.rs

---
+++ main.rs
@@ -0,0 +1,16 @@
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    "Hi there!"
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
